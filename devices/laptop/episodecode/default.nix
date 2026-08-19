b96d95758b33805c3d96eb5e649ad42f3538c984
abeddx <abbe_92@live.com>
2026-08-20 00:22:57 +0300

    .

--- devices/laptop/episodecode/default.nix
+++ devices/laptop/episodecode/default.nix
@@ -54,7 +54,6 @@
       packages = [
         "flathub:app/app.zen_browser.zen//stable"
         "flathub:app/com.github.tchx84.Flatseal//stable"
-        "flathub:app/com.usebottles.bottles//stable"
       ];
     };
   };
